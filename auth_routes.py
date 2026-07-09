a15f9cfe016dac7e16e6597420c35e35386ed500
Yousef Mahrous <yousefzentral@gmail.com>
2026-07-09 16:54:54 +0300

    init

--- auth_routes.py
+++ auth_routes.py
@@ -86,6 +86,7 @@ def delete_account(
     subscriptions = session.exec(select(models.Subscription).where(models.Subscription.user_id == user.id)).all()
     feedbacks = session.exec(select(models.Feedback).where(models.Feedback.user_id == user.id)).all()
     vendors = session.exec(select(models.Vendor).where(models.Vendor.user_id == user.id)).all()
+    password_reset_tokens = session.exec(select(models.PasswordResetToken).where(models.PasswordResetToken.user_id == user.id)).all()
 
     for doc in documents:
         storage_client.delete_from_storage(doc.filename)
@@ -102,6 +103,9 @@ def delete_account(
 
     for vendor in vendors:
         session.delete(vendor)
+    
+    for token in password_reset_tokens:
+        session.delete(token)
 
     session.delete(user)
     session.commit()
