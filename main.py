76adca784cd4b613728d7d052437e3b38a289a9f
Yousef Mahrous <yousefzentral@gmail.com>
2026-07-11 15:25:18 +0300

    test

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ origins = [origin.strip() for origin in allowed_origins_str.split(",") if origin
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # origins
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"],
