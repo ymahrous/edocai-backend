3ca7379e30eed030221f300572a8cdedab8a085c
Yousef Mahrous <yousefzentral@gmail.com>
2026-09-16 21:25:24 +0300

    fix: main issue

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ origins = [origin.strip() for origin in allowed_origins_str.split(",") if origin
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"],
     expose_headers=["*"]
